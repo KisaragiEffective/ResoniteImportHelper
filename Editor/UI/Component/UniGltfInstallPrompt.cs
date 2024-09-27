93319832d2a675f5f68835038ee66e8ac4f28154
Kisaragi Marine <kisaragi.effective@gmail.com>
2024-09-28 03:24:38 +0900

    chore: fix warnings

--- Editor/UI/Component/UniGltfInstallPrompt.cs
+++ Editor/UI/Component/UniGltfInstallPrompt.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using ResoniteImportHelper.UI.Localize;
 using UnityEngine.UIElements;
 
@@ -5,34 +6,33 @@ namespace ResoniteImportHelper.UI.Component
 {
     internal sealed class UniGltfInstallPrompt: VisualElement
     {
-        internal UniGltfInstallPrompt(ILocalizedTexts lang)
+        internal UniGltfInstallPrompt([UsedImplicitly] ILocalizedTexts lang)
         {
-            var rootVisualElement = this;
 #if !RIH_HAS_UNI_GLTF
-            {
-                var message =
+            var rootVisualElement = this;
+            
+            var message =
 #if RIH_HAS_UNI_GLTF_ANY
-                    lang.ConflictingVersionOfUniGLTFIsInstalled()
+                lang.ConflictingVersionOfUniGLTFIsInstalled()
 #else
-                    lang.UniGLTFIsNotInstalled()
+                lang.UniGLTFIsNotInstalled()
 #endif
-                    ;
-                rootVisualElement.Add(
-                    new HelpBox(message, HelpBoxMessageType.Error)
-                );
+                ;
+            rootVisualElement.Add(
+                new HelpBox(message, HelpBoxMessageType.Error)
+            );
+            {
+                var button = new Button(() =>
                 {
-                    var button = new Button(() =>
-                    {
-                        Application.OpenURL("https://github.com/vrm-c/UniVRM/releases/tag/v0.125.0");
-                    });
-                    button.Add(new Label(lang.OpenInstallationPageForUniGLTF()));
-                    rootVisualElement.Add(button);
-                }
-                {
-                    var button = new Button(PackageManagerProxy.InstallUniGLTF);
-                    button.Add(new Label(lang.InstallUniGLTFAutomatically()));
-                    rootVisualElement.Add(button);
-                }
+                    Application.OpenURL("https://github.com/vrm-c/UniVRM/releases/tag/v0.125.0");
+                });
+                button.Add(new Label(lang.OpenInstallationPageForUniGLTF()));
+                rootVisualElement.Add(button);
+            }
+            {
+                var button = new Button(PackageManagerProxy.InstallUniGLTF);
+                button.Add(new Label(lang.InstallUniGLTFAutomatically()));
+                rootVisualElement.Add(button);
             }
 #endif
         }
