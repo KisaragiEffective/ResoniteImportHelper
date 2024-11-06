914ed2526404b39f1cdca0411192b99487284b03
Kisaragi Marine <kisaragi.effective@gmail.com>
2024-11-06 22:19:32 +0900

    chore: obsolete [NotPublicAPI]

--- Editor/Marker/NotPublicAPI.cs
+++ Editor/Marker/NotPublicAPI.cs
@@ -1,9 +1,12 @@
 #nullable enable
+using System;
+
 namespace ResoniteImportHelper.Marker
 {
     [NotPublicAPI]
+    [Obsolete("Default API stability is set to unstable; this attribute is going to be removed.")]
     public class NotPublicAPI : System.Attribute
     {
-        
+
     }
 }
