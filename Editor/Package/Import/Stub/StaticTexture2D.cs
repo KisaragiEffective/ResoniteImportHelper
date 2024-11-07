9674fa05e1337f2a1a1d631097fe25a29b63ac29
Kisaragi Marine <kisaragi.effective@gmail.com>
2024-11-08 06:00:01 +0900

    reflect KisaragiEffective#197

--- Editor/Package/Import/Stub/StaticTexture2D.cs
+++ Editor/Package/Import/Stub/StaticTexture2D.cs
@@ -1,10 +1,8 @@
-using ResoniteImportHelper.Marker;
 using UnityEngine;
 
 namespace ResoniteImportHelper.Editor.Package.Import.Stub
 {
     #if UNITY_EDITOR
-    [NotPublicAPI]
     public class StaticTexture2D : MonoBehaviour
     {
         public Texture2D texture;
